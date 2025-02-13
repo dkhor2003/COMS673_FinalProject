515a0663e3572634ed73e2b277f41e6d683e39ad
dkhor2003 <khordylan2003@gmail.com>
2025-02-12 21:25:32 -0600

    Initial code

---
+++ deploy_mujoco.py
@@ -0,0 +1,185 @@
+import time
+import mujoco.viewer
+import mujoco
+import numpy as np
+import torch
+import yaml
+import os
+import csv
+
+def get_gravity_orientation(quaternion):
+    qw = quaternion[0]
+    qx = quaternion[1]
+    qy = quaternion[2]
+    qz = quaternion[3]
+
+    gravity_orientation = np.zeros(3)
+
+    gravity_orientation[0] = 2 * (-qz * qx + qw * qy)
+    gravity_orientation[1] = -2 * (qz * qy + qw * qx)
+    gravity_orientation[2] = 1 - 2 * (qw * qw + qz * qz)
+
+    return gravity_orientation
+
+
+def pd_control(target_q, q, kp, target_dq, dq, kd):
+    """ Calculates torques from position commands """
+    return (target_q - q) * kp + (target_dq - dq) * kd
+    
+if __name__ == "__main__":
+    # get config file name from command line
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_file", type=str, help="config file name in the config folder")
+    args = parser.parse_args()
+    config_file = args.config_file
+    data_root_dir = "data"
+    
+    with open(config_file, "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+        policy_path = config["policy_path"]
+        xml_path = config["xml_path"]
+        
+        log_on = config["log_on"]
+
+        simulation_duration = config["simulation_duration"]
+        simulation_dt = config["simulation_dt"]
+        control_decimation = config["control_decimation"]
+
+        kps = np.array(config["kps"], dtype=np.float32)
+        kds = np.array(config["kds"], dtype=np.float32)
+
+        default_angles = np.array(config["default_angles"], dtype=np.float32)
+
+        ang_vel_scale = config["ang_vel_scale"]
+        dof_pos_scale = config["dof_pos_scale"]
+        dof_vel_scale = config["dof_vel_scale"]
+        action_scale = config["action_scale"]
+        cmd_scale = np.array(config["cmd_scale"], dtype=np.float32)
+
+        num_actions = config["num_actions"]
+        num_obs = config["num_obs"]
+        
+        cmd = np.array(config["cmd_init"], dtype=np.float32)
+
+    # define context variables
+    action = np.zeros(num_actions, dtype=np.float32)
+    target_dof_pos = default_angles.copy()
+    obs = np.zeros(num_obs, dtype=np.float32)
+    
+    counter = 0
+
+    # One control one actual
+    m = mujoco.MjModel.from_xml_path(xml_path)
+    d = mujoco.MjData(m)
+    m.opt.timestep = simulation_dt
+    
+    if log_on:
+        
+        if not os.path.exists(data_root_dir):
+            os.mkdir(data_root_dir)
+            traj_num = 1
+        else:
+            trajs = os.listdir(data_root_dir)
+            traj_num = len(list(filter(lambda file: file.endswith(".csv"), trajs))) + 1
+            
+        csvfile = open(os.path.join(data_root_dir, f"{traj_num}.csv"), "w", newline="")
+        traj_logger = csv.writer(csvfile)
+        
+        joint_names = []
+        dofs_q = []
+        dofs_dq = []
+        dofs_torque = []
+        
+        for i in range(m.njnt):
+            joint_names.append(mujoco.mj_id2name(m, mujoco.mjtObj.mjOBJ_JOINT, i))
+
+        root_body = joint_names[0]
+        dofs = joint_names[1:]
+        
+        root_body_q = [f"{root_body}_qw",
+                       f"{root_body}_qx",
+                       f"{root_body}_qy",
+                       f"{root_body}_qz"]
+
+        root_body_dq = [f"{root_body}_wx",
+                        f"{root_body}_wy",
+                        f"{root_body}_wz"]
+        
+        for dof in dofs:
+            dofs_q.append(f"{dof}_q")
+            dofs_dq.append(f"{dof}_dq")
+            dofs_torque.append(f"{dof}_torque")
+        
+        robot_command = ["vx_command", "vy_command", "yaw_command"]
+        
+        header = root_body_q + dofs_q + root_body_dq + dofs_dq + dofs_torque + robot_command
+        traj_logger.writerow(header)  
+
+    # load policy
+    policy = torch.jit.load(policy_path)
+    
+    with mujoco.viewer.launch_passive(m, d) as viewer:
+        
+        # Close the viewer automatically after simulation_duration wall-seconds.
+        start = time.time()
+        while viewer.is_running() and time.time() - start < simulation_duration:
+            
+            step_start = time.time()
+            tau = pd_control(target_dof_pos, d.qpos[7:], kps, np.zeros_like(kds), d.qvel[6:], kds)
+            d.ctrl[:] = tau
+            
+            # mj_step can be replaced with code that also evaluates
+            # a policy and applies a control signal before stepping the physics.
+            mujoco.mj_step(m, d)
+        
+            counter += 1
+            if counter % control_decimation == 0:
+                
+                # Log joint states (excluding base xyz pos and linear xyz velocity)
+                if log_on:
+                    traj_logger.writerow(np.concatenate((d.qpos[3:], d.qvel[3:], tau, cmd)))
+                
+                # Apply control signal here.
+
+                # create observation
+                qj = d.qpos[7:]
+                dqj = d.qvel[6:]
+                quat = d.qpos[3:7]
+                omega = d.qvel[3:6]
+
+                qj = (qj - default_angles) * dof_pos_scale
+                dqj = dqj * dof_vel_scale
+                gravity_orientation = get_gravity_orientation(quat)
+                omega = omega * ang_vel_scale
+
+                period = 0.8
+                count = counter * simulation_dt
+                phase = count % period / period
+                sin_phase = np.sin(2 * np.pi * phase)
+                cos_phase = np.cos(2 * np.pi * phase)
+
+                obs[:3] = omega
+                obs[3:6] = gravity_orientation
+                obs[6:9] = cmd * cmd_scale
+                obs[9 : 9 + num_actions] = qj
+                obs[9 + num_actions : 9 + 2 * num_actions] = dqj
+                obs[9 + 2 * num_actions : 9 + 3 * num_actions] = action
+                obs[9 + 3 * num_actions : 9 + 3 * num_actions + 2] = np.array([sin_phase, cos_phase])
+                obs_tensor = torch.from_numpy(obs).unsqueeze(0)
+                # policy inference
+                action = policy(obs_tensor).detach().numpy().squeeze()
+                # transform action to target_dof_pos
+                target_dof_pos = action * action_scale + default_angles
+
+            # Pick up changes to the physics state, apply perturbations, update options from GUI.
+            viewer.sync()
+
+            # Rudimentary time keeping, will drift relative to wall clock.
+            time_until_next_step = m.opt.timestep - (time.time() - step_start)
+            if time_until_next_step > 0:
+                time.sleep(time_until_next_step)
+    
+    if log_on:
+        csvfile.close()
