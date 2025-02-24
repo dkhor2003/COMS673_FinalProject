1d87cf753b9a17bf7adccc2b4917228a74ca657d
dkhor2003 <khordylan2003@gmail.com>
2025-02-24 10:09:27 -0600

    Piece of code for simulating trajectories we collected

---
+++ simulate_traj.py
@@ -0,0 +1,27 @@
+import mujoco
+import mujoco.viewer
+import pandas as pd
+import numpy as np
+
+def simulate(xml_path, traj_file, dt=0.002):
+    m = mujoco.MjModel.from_xml_path(xml_path)
+    d = mujoco.MjData(m)
+    m.opt.timestep = dt
+    df = pd.read_csv(traj_file)
+    with mujoco.viewer.launch_passive(m, d) as viewer:
+        for id, row in df.iterrows():
+            qpos = row[:19].to_numpy()
+            qvel = row[19:37].to_numpy()
+            qacc = row[37:55].to_numpy()
+            fallover = int(row[55]) 
+            d.qpos = qpos
+            d.qvel = qvel
+            d.qacc = qacc
+            mujoco.mj_forward(m, d)
+            viewer.sync()
+       
+    
+if __name__ == "__main__":
+    xml_path = "model/scene.xml"
+    traj_file = "data/1.csv"
+    simulate(xml_path, traj_file)
